0bcf10e266eff62e693949b8eb14ecfd96fd9a62
Arik Fraimovich <arik@arikfr.com>
2010-02-13 15:14:27 +0200

    Added config; Added .gitignore

---
+++ config/initializers/config.rb
@@ -0,0 +1,8 @@
+require "ostruct"
+
+App = OpenStruct.new({
+  :oauth => OpenStruct.new({
+    :token => "1QDP0tuZDX50pwXWHl9u2w",
+    :secret => "QljFqIp6QBwqCAax1sYoH5IQ60VkPSrTvhZaoi5ZRs"
+  })
+})
