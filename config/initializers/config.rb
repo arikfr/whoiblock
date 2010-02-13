3264541e8452121076a5b50d74a3369f76b05319
Arik Fraimovich <arik@arikfr.com>
2010-02-13 15:16:57 +0200

    Updated tokens

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,7 +2,7 @@ require "ostruct"
 
 App = OpenStruct.new({
   :oauth => OpenStruct.new({
-    :token => "1QDP0tuZDX50pwXWHl9u2w",
-    :secret => "QljFqIp6QBwqCAax1sYoH5IQ60VkPSrTvhZaoi5ZRs"
+    :token => "2cC9H9ZrNkEfbBfFQB4TNA",
+    :secret => "BMLT5SxDLy6Ab7CblNiyRtZDBG1MZWaaICc1Ql1uZFQ"
   })
 })
