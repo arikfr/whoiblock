55d9aa6518e724c6ac1a67ba3d88cf8671a2323f
Arik Fraimovich <arik@arikfr.com>
2010-02-13 15:32:47 +0200

    Minor changes

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,5 +1,10 @@
 require "ostruct"
 
+#
+# Don't bother using the tokens below - they are no longer vaild...
+# Create your own at: http://twitter.com/oauth_clients
+#
+
 App = OpenStruct.new({
   :oauth => OpenStruct.new({
     :token => "2cC9H9ZrNkEfbBfFQB4TNA",
